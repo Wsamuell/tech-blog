492567b11f598ca9abb54a0533757877da75a83a
samuel Odubamowo <wemsamuel@gmail.com>
2021-06-20 22:11:02 -0700

    sequelize update

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: 'localhost',
-    dialect: 'mysql',
-    // for heroku i believe
-    port: 3306
-});
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+        host: 'localhost',
+        dialect: 'mysql',
+        // for heroku i believe
+        port: 3306
+    });
 
 module.exports = sequelize
